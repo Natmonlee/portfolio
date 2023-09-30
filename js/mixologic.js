1539fdfb7153f4756ce6d09173f2639810902386
Natalie Moran <nataliemoran89@live.co.uk>
2023-09-30 18:01:07 +0100

    Amend file structure

--- js/mixologic.js
+++ js/mixologic.js
@@ -1,54 +1,121 @@
-let glassResultSection = document.getElementById("glassResult");
-let ingredientResultSection = document.getElementById("ingredientResult");
-let cocktailResultSection = document.getElementById("cocktailResult");
-let postResultSection = document.getElementById("postResult");
-let endpointText = document.getElementById("endpointText");
-let example = document.getElementById("example");
-let responseStatus = document.getElementById("responseStatus");
-let responseBody = document.getElementById("responseBody");
-
-const dropItem1 = document.getElementById('dropItem1');
-const dropItem2 = document.getElementById('dropItem2');
-const dropItem3 = document.getElementById('dropItem3');
-const dropItem4 = document.getElementById('dropItem4');
-const clickArea1 = document.getElementById('clickArea1');
-const clickArea2 = document.getElementById('clickArea2');
-const clickArea3 = document.getElementById('clickArea3');
-const clickArea4 = document.getElementById('clickArea4');
-
-
-const addDropdownOnClick = (clickArea, dropItem) => {
-    clickArea.addEventListener("click", () => dropItem.classList.toggle("dropDownActive"));
-};
-
-addDropdownOnClick(clickArea1, dropItem1);
-addDropdownOnClick(clickArea2, dropItem2);
-addDropdownOnClick(clickArea3, dropItem3);
-addDropdownOnClick(clickArea4, dropItem4);
-
+const textInput = document.getElementById("inputBox");
+const resultsDiv = document.getElementById('resultsList');
+let flipInstructionHeight;
 
 const getResult = async () => {
-    const glassEndpoint = 'http://13.41.54.243/v1/cocktail-glasses'
-    const ingredientEndpoint = 'http://13.41.54.243/v1/cocktail-ingredients'
-
+    resultsDiv.innerHTML = "";
+    const textInputValue = inputBox.value;
+    const searchMethod = document.querySelector("input[name='cocktail']:checked").value;
+    const endpoint = "https://api.api-ninjas.com/v1/cocktail?" + searchMethod + "=" + textInputValue;
     try {
-        let headers = new Headers();
-        headers.append("Content-type", "application/json");
-        headers.append("Accept", "application/json");
+        const response = await fetch(endpoint, {
+            method: "GET",
+            headers: {
+                "X-Api-Key": "E6CyLZAZwxU0FySnN6w0IQ==Afe3GoBzy0YapmO8"
+            }
+        });
+        if (response.ok) {
+            const finalResponse = await response.json();
+            if (finalResponse.length === 0) {
+                resultsList.innerHTML = "Sorry, your search returned no results!";
+            }
+            else {
+                inputBox.value = '';
+                for (const number of finalResponse) {
+                    let ingredientsList = '';
+                    for (let i = 0; i < number.ingredients.length; i++) {
+                        if (i === number.ingredients.length - 1) {
+                            ingredientsList += `•${number.ingredients[i]}`;
+                        } else {
+                            ingredientsList += `•${number.ingredients[i]}<br>`;
+                        }
+                    }
 
-        const glassResponse = await fetch(glassEndpoint, {
-            method: "GET"
-        });
-        const ingredientResponse = await fetch(ingredientEndpoint, {
-            method: "GET"
-        });
-        if (glassResponse.ok) {
-            const glassResultText = await glassResponse.json();
-            glassResult.innerHTML = JSON.stringify(glassResultText);
-        }
-        if (ingredientResponse.ok) {
-            const ingredientResultText = await ingredientResponse.json();
-            ingredientResultSection.innerHTML = JSON.stringify(ingredientResultText);
+                    let cocktailCard = document.createElement('div');
+                    cocktailCard.classList.add("cocktailCard");
+
+                    let cardInner = document.createElement('div');
+                    cardInner.classList.add("innerDiv", "transformAnimation", "toResize");
+
+                    let cardFront = document.createElement('div');
+                    cardFront.classList.add("front", "side");
+
+                    let frontContent = document.createElement('div');
+                    frontContent.classList.add("content");
+
+                    let cocktailNameFront = document.createElement('h1');
+                    cocktailNameFront.innerHTML = `${number.name}`;
+                    let subheadingFront = document.createElement('h2');
+                    subheadingFront.innerHTML = 'Ingredients';
+                    let cocktailIngredients = document.createElement('p');
+                    cocktailIngredients.classList.add("ingredients");
+                    cocktailIngredients.innerHTML = ingredientsList;
+                    let flipInstructionFront = document.createElement('div');
+                    flipInstructionFront.classList.add("flipInstruction");
+                    flipInstructionFront.innerHTML = 'Flip for instructions';
+
+                    let cardBack = document.createElement('div');
+                    cardBack.classList.add("back", "side");
+
+                    let backContent = document.createElement('div');
+                    backContent.classList.add("content");
+
+                    let cocktailNameBack = document.createElement('h1');
+                    cocktailNameBack.innerHTML = `${number.name}`;
+                    let subheadingBack = document.createElement('h2');
+                    subheadingBack.innerHTML = 'Instructions';
+                    let cocktailInstructions = document.createElement('p');
+                    cocktailInstructions.classList.add("instructions");
+                    cocktailInstructions.innerHTML = number.instructions;
+                    let flipInstructionBack = document.createElement('div');
+                    flipInstructionBack.classList.add("flipInstruction");
+                    flipInstructionBack.innerHTML = 'Flip for ingredients';
+
+                    resultsDiv.appendChild(cocktailCard);
+                    cocktailCard.appendChild(cardInner);
+
+                    cardInner.appendChild(cardFront);
+                    cardInner.appendChild(cardBack);
+
+                    cardFront.appendChild(frontContent);
+                    cardBack.appendChild(backContent);
+
+                    frontContent.appendChild(cocktailNameFront);
+                    frontContent.appendChild(subheadingFront);
+                    frontContent.appendChild(cocktailIngredients);
+                    cardFront.appendChild(flipInstructionFront);
+
+                    backContent.appendChild(cocktailNameBack);
+                    backContent.appendChild(subheadingBack);
+                    backContent.appendChild(cocktailInstructions);
+                    cardBack.appendChild(flipInstructionBack);
+
+                    function rotateCard() {
+                        cardInner.classList.toggle("rotate");
+                    }
+                    cocktailCard.addEventListener("click", rotateCard);
+                }
+
+                function sizeDivs() {
+                    let largestElementHeight = 0;
+                    for (element of resultsDiv.getElementsByClassName('content')) {
+                        let currentHeight = Math.ceil(element.clientHeight);
+                        if (currentHeight > largestElementHeight) {
+                            largestElementHeight = currentHeight;
+                        }
+                    }
+
+                    let flipInstruction = resultsDiv.getElementsByClassName('flipInstruction');
+                    let firstFlipInstruction = flipInstruction[0];
+
+                    let allInnerDivs = resultsDiv.getElementsByClassName('innerDiv');
+                    for (let element of allInnerDivs) {
+                        element.style.height = largestElementHeight + firstFlipInstruction.clientHeight + 'px';
+                    }
+                }
+                sizeDivs();
+                window.onresize = sizeDivs;
+            }
         }
     }
     catch (error) {
@@ -56,89 +123,16 @@ const getResult = async () => {
         throw new Error('Request failed!');
     };
 }
-
-getResult();
-
-const searchCocktails = async () => {
-    let cocktailEndpoint = "http://13.41.54.243/v1/cocktail-recipes?";
-    let nameInput = document.getElementById("name").value;
-    let alcoholicTrueInput = document.getElementById("true").checked;
-    let alcoholicFalseInput = document.getElementById("false").checked;
-    let glassInput = document.getElementById("glass").value;
-    let ingredientsInput = document.getElementById("ingredients").value;
-    let minIngredientsInput = document.getElementById("minIngredients").options.selectedIndex;
-    let maxIngredientsInput = document.getElementById("maxIngredients").options.selectedIndex;
-
-    if (nameInput) {
-        cocktailEndpoint += "name=" + nameInput + "&";
+textInput.addEventListener("keypress", event => {
+    if (event.key === "Enter") {
+        getResult();
     }
-    if (alcoholicTrueInput) {
-        cocktailEndpoint += "alcoholic=true&";
+})
+radioButtons.addEventListener("keypress", event => {
+    if (event.key === "Enter") {
+        getResult();
     }
-    if (alcoholicFalseInput) {
-        cocktailEndpoint += "alcoholic=false&";
-    }
-    if (glassInput) {
-        cocktailEndpoint += "glass=" + glassInput + "&";
-    }
-    if (ingredientsInput) {
-        cocktailEndpoint += "ingredients=" + ingredientsInput + "&";
-    }
-    if (minIngredientsInput) {
-        cocktailEndpoint += "minIngredients=" + minIngredientsInput + "&";
-    }
-    if (maxIngredientsInput) {
-        cocktailEndpoint += "maxIngredients=" + maxIngredientsInput + "&";
-    }
+})
+const submitButton = document.getElementById("submitButton");
+submitButton.addEventListener("click", getResult);
 
-    try {
-        let cocktailResponse = await fetch(cocktailEndpoint, {
-            method: "GET"
-        });
-
-        if (cocktailResponse.ok) {
-            let cocktailResultText = await cocktailResponse.json();
-            endpointText.innerHTML = "Endpoint: " + cocktailEndpoint;
-            example.innerHTML = "";
-            cocktailResultSection.innerHTML = JSON.stringify(cocktailResultText, null, 4);
-        }
-    }
-    catch (error) {
-        console.log(error);
-        throw new Error('Request failed!');
-    };
-}
-
-
-const addCocktail = async () => {
-    const postEndpoint = "http://13.41.54.243/v1/add-cocktail";
-
-    try {
-        let headers = new Headers();
-        headers.append("Content-type", "application/json");
-        headers.append("Accept", "application/json");
-
-        let postInput = document.getElementById("postInput").innerHTML;
-        let postInputFormatted = postInput.replaceAll('\n', '');
-        let postResponse = await fetch(postEndpoint, {
-            method: "POST",
-            headers: headers,
-            body: postInputFormatted
-        });
-        
-        let postResponseBody = await postResponse.json();
-        responseStatus.innerHTML = `${postResponse.status}<br>${postResponse.statusText}`;
-        console.log(postResponseBody);
-        responseBody.innerHTML = JSON.stringify(postResponseBody);
-    }
-    catch (error) {
-        console.log(error);
-        throw new Error('Request failed!');
-    };
-}
-
-const getSubmitButton = document.getElementById("getCocktailSubmit");
-getSubmitButton.addEventListener("click", searchCocktails);
-
-const postSubmitButton = document.getElementById("postCocktailSubmit");
-postSubmitButton.addEventListener("click", addCocktail);
